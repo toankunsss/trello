4d3396f68bc08b51781788469d59b313ee30e6b3
toan <nguyenvantoanmnsh@gmail.com>
2025-06-09 21:08:25 +0700

    add css bassline

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,14 @@ module.exports = {
     'react/prop-types': 0,
     'react/display-name': 0,
 
+    'no-restricted-imports': [
+      'error',
+      {
+        'patterns': [{ 'regex': '^@mui/[^/]+$' }]
+      }
+    ],
+
+
     'no-console': 1,
     'no-lonely-if': 1,
     'no-unused-vars': 1,
