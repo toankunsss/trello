9e15e71cef73844debc6a9d12d6d972dae4a1796
toan <nguyenvantoanmnsh@gmail.com>
2025-06-14 07:57:52 +0700

    refactor folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,57 @@
-import { useState } from 'react'
 import Button from '@mui/material/Button'
+import { useColorScheme } from "@mui/material/styles"
+import { Box, useMediaQuery } from '@mui/material'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightMode from '@mui/icons-material/LightMode'
+import DarkModeOutlined from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 
-function App() {
-  const [count, setCount] = useState(0)
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
+
+  const handleChange = (event) => {
+    const selecedMode = event.target.value
+    setMode(selecedMode)
+  };
 
   return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <LightMode fontSize='small' /> Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeOutlined fontSize='small' /> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <SettingsBrightnessIcon fontSize='small' /> System
+          </div>
+        </MenuItem>
+
+      </Select>
+    </FormControl>
+  );
+}
+
+function App() {
+  return (
     <>
+      <ModeSelect />
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,27 @@
 import { createTheme } from "@mui/material/styles"
 import { red } from "@mui/material/colors"
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
 
 // Create a theme instance.
-const theme = createTheme({
-  cssVariables: true,
-  palette: {
-    primary: {
-      main: "#556cd6"
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          main: '#ff5252',
+        }
+      },
+      spacing: (factor) => `${0.25 * factor}rem`
     },
-    secondary: {
-      main: "#19857b"
-    },
-    error: {
-      main: red.A400
-    },
+    dark: {
+      palette: {
+        primary: {
+          main: '#000',
+        }
+      },
+      spacing: (factor) => `${0.25 * factor}rem`
+    }
   }
-})
+});
 
 export default theme;
